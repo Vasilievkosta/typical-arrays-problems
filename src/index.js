6a78bc4369fac1fefb3bee04b8f59a7ea2f04f0b
Konstantin <vasilievkosta@gmail.com>
2020-09-29 23:05:36 +0300

    refactor: three modified src

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 
 exports.min = function min (array) {	
-	if (array === '' || array === ['']) {
+	if (array === '' || array === []) {
 		return 0;
 	}
   return Math.min(...array);
 }
 
 exports.max = function max (array) {
-	if (array === '' || array === ['']) {
+	if (array === '' || array === []) {
 		return 0;
 	}
   return Math.max(...array);
 }
 
 exports.avg = function avg (array) {
-	if (array === '' || array === ['']) {
+	if (array === '' || array === []) {
 		return 0;
 	}
 	let result = array.reduce(function(sum, elem) {
